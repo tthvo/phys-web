ca6a97d602368460fa8ee7ad9796d148672422a7
Thuan Vo <thuan.vo@ubc.ca>
2021-02-15 18:46:53 -0800

    edit contents for pre lab 4

--- phys_book/_build/jupyter_execute/contents/lab4/prelab4.py
+++ phys_book/_build/jupyter_execute/contents/lab4/prelab4.py
@@ -1,34 +1,44 @@
 # Pre Lab 4
 
-Here is your help on Pre Lab 4
+Welcome back everyone! Here is your help on Pre Lab 4
 ___ 
 
 
 ## Part 1
 
-### Question 1
+````{margin}
+```{admonition} Hot tip!
+:class: warning
+The answer is right in the lab manual
+```
+````
 - Basically, you tell me what Coulomb's Law is.
+- What I want from you is: **brief description** and **main formula**.
 
-- What I want from you is: ***brief description*** and ***main formula***.
-
-```{tip}
+```{note}
 You can go to your note and copy them out! It's a law with a formula similar to that of Newton's attractive force.
 ```
 
-### Question 2
+````{margin}
+```{tip}
+You can try it yourself!
+```
+````
 
-- Well, this is a conceptual question that I believe everyone knows how to do it! It is just common sense and you can even test it yourself.
+- Next, this is a conceptual question. There is nothing tricky here!
+- This is a very common question on **electrostatic induction**.
 
-- There is nothing tricky here!
 
 
 ## Part 2
 
-- Until now, everyone should be good with dealing with algebra and units.
-
-- This question is just simple calculations by **destroying unwanted units**.
-
+````{margin}
 ```{tip}
 Write out the units of each quantity!
 ```
+````
+
+- Until now, everyone should be good with dealing with algebra and units.
+- This question is just simple calculations by **destroying unwanted units**.
+
 
