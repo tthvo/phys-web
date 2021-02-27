ce8b0bb32276fa820fa38eb2f8f7d1b797fe9782
Thuan Vo <thuan.vo@ubc.ca>
2021-02-26 16:43:05 -0800

    adding expectation page

---
+++ phys_book/_build/jupyter_execute/intros/expectation.py
@@ -0,0 +1,79 @@
+# Expectations
+
+Here are the things I want you to learn from the lab!
+___
+
+## Overview
+- I know this is really subjective as I base everything on what I learnt and experienced. So, before we begin, let me mark my word here.
+
+> Skills are what you need. 
+> It is your choice to decide your way to learn it.
+
+- In other words, you don't have to follow me. Just know those skills are the essential skills!
+
+## What you get from lab
+
+- Now, I want to go a little deeper into what I mean.
+
+### Descriptions
+
+- Whenever you are asked to **observe** a physical events, keep in mind these things.
+    - What you see is different from what others see. **Your view is unique**.
+    - What you see might not be what is really happening. **You need to evaluate it**.
+    - What you report is what you see after making adjustments. **Do not use assumptions**.
+
+- Do your setences need to be elegant and complex? <mark>No! Information matters more!</mark>
+
+```{attention}
+Observation is the crucial steps in science. But the eyes are easily deceived.
+```
+
+### Explanations
+
+- Whenever you are asked to **explain** a physics events, keep in mind these things.
+    - Your first thoughts of the matter is not always the ultimately correct one.
+    - Your second thoughts might be not.
+    - Only when you think about it so many times, you will get the right idea. **Dedication matters**.
+    - Explain with all your understanding and efforts. 
+    - Others' idea might be right. You can use it but in your own words.
+    - Other's idea might be wrong. If you suspect it, investigate it! **Do not trust what you cannot trust**. 
+
+- Again, do your setences need to be elegant and complex? <mark>No! Information matters more!</mark>
+
+```{caution}
+Your prof's words might sound elegant but it is not coming your own mind!
+```
+### Data Analysis
+
+- This is an important part that you definitely need to learn.
+- What labs are all about: **Data**.
+- Data are presented in raw (meaningless columns and rows).
+- Your tasks: Transform the data into something useful and draw helpful information from that.
+- Your next task: Explain whether your dataset is reasonable? Anything abnormal?
+    - What if my data is perfect? Great! Just go on with it!
+    - What if my data is screwed? No worries! Look for the error! And note it down.
+    - What if my data is weird? Well, you might have discovered something! Note it donw.
+
+- Your final task: Give your analysis on the data.
+    - Comparing columns: E.g. "within experimental" meanings "there is a overlap region of those compared numbers".
+    - Graph: E.g. Given a linear function, what does the slope represent? Can it be use to do something? Example in lab 2
+    - Abnormal things: E.g. My data is negative but most of my friends have positive data. What might happen?
+
+```{warning}
+A simple yes or no does not give anything useful!
+```
+
+## What you contribute to lab
+
+- Let's talk about what you do for the labs and for me.
+
+### Diversity
+
+- What I am having from you all is the unique understanding and views on the subject from every individual.
+- You bring new ideas into the table. You bring up problems that we, TA, have not encountered.
+- You bring new ways of solving problems that beyond the answer sheets.
+
+### Problems
+
+- Yes, problems! You made problems for me solve such that the system can be improved.
+- Your feedbacks and complains will always be heard.
