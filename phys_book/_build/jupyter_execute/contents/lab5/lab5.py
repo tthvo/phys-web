1555f3a1323d9b5135627533c4b5ea913e378cea
Thuan Vo <thuan.vo@ubc.ca>
2021-03-04 15:13:17 -0800

    finish part1,2 of lab5 and set layout for part3

--- phys_book/_build/jupyter_execute/contents/lab5/lab5.py
+++ phys_book/_build/jupyter_execute/contents/lab5/lab5.py
@@ -1,6 +1,107 @@
 # Lab 5
+Here are some stuffs for Lab 5!
+___
 
-## Coming Soon!
+
+## Overview
+
+- This lab might seem a little too long but eventually it is not that bad. 
+
+- You need an image editor to annotate your screenshots. E.g. Paint, Preview, PS.
+
+- We will need to plot 2 graphs from LoggerPro.
+
+- Excel work will start this week.
+
+```{attention}
+Please read the instructions in the manual on how to use the simulator!
+```
+
+## Part 1: The Electric Dipole
+````{margin}
+```{tip}
+Electric field lines are going from higher potential to lower potential and perpendicular to equipotential lines.
+```
+````
+- Here, all you need to do is to place <mark><b>2 opposite charges</b></mark> on 2 halves of the surface (one left and one right).
+- Then, you use the device with the **cross-hair** to draw the equipotential lines. How? **Click the pencil**.
+- Make sure you follow the required steps (not necessarily exact). In this case, it is $\pm3(V)$.
+- Then, you need to draw **Electric field Lines** given the equipotential lines.
+- Here a demo for you to follow:
+
+```{figure} ../../images/lab5/part1.png
+:scale: 50
+```
+
+```{warning}
+Every image contains metadata, including author and created date. So, I would know whether you did it or copied somewhere.
+```
+
+## Part 2: Pair of Like Charges
+````{margin}
+```{tip}
+In case you don't see the potential you're looking for, move the device up and down from the middle.
+```
+````
+- The procedure is similar to part 1. The only difference is that you place <mark><b>2 same charges</b></mark> on 2 halves of the surface.
+- Make sure you follow the required steps (not necessarily exact). In this case, it is $\pm1(V)$.
+- Then, draw the **Electric field lines**. Here is a demo for you:
+
+```{figure} ../../images/lab5/part2.png
+:scale: 45
+```
+```{caution}
+Here, we have 2 like-charges. So be careful with the direction of the electric field lines.
+```
+
+
+## Part 3: A Line of Charge
+```{tip}
+Keep track on the number of charges. Usually ~55-58 charges.
+```
+- This part requires you to assemble a **line of charges**. Make sure to<font color='red'><b>fill the entire browswer length</b></font>.
+```{seealso}
+Filling the entire length ensures that it behaves like a line of charges. Otherwise, some discrepancies might happen.
+```
+- Here is demo of the setup for you to follow:
+```{figure} ../../images/lab5/part3.png
+:scale: 50
+```
+
+### Section 1: Find the charge per unit length λ
+
+- You have the equation:
+$$\lambda = \frac{n * q}{L} (C/m)$$
+where n(number of charges), q(charge per point charge), L(length of line)
+
+```{tip}
+q is given in the simulator! Find it! Use nC or C.
+```
+- So, you need to keep track on how many points you put in. Also, to find the length of the line, **use the scale with the plus symbol at the tip**.
+
+
+### Section 2: Find estimated eletrical field strength Ex (V/m) at a point
+
+### Section 3: Find estimated potential Vx (V) at a point
+
+
+## Discussion
+
+- We do not have a discussion part for this lab. But there are few things you need to remember.
+- There are several questions asking you to describe the pattern. So, please give your best observations. It must be your own.
+
+```{tip}
+One important concepts: The closer the equipotential lines are, the stronger the eletric field in those areas.
+```
+
+## Excel Work
+
+- **Please keep track on the schedule. Rules must also be followed.**
+- For lab 3, [Click Here](https://docs.google.com/spreadsheets/d/1cmIqJLanpAlZ6kQmgPFBkUN9hX1Z3Y3iBFWBpIDyVzk/edit?usp=sharing).
+- For lab 12, [Click Here](https://docs.google.com/spreadsheets/d/1nIeibzET3l4k8WNUURpX4TXzFUnij_JojNM6cykBM6o/edit?usp=sharing).
+
+## Recordings
+Coming Soon!
 
 
 ```{toctree}
