ff1e8afea1c80dbb8b2e9ac113750b4f9c9bab8f
Thuan Vo <thuan.vo@ubc.ca>
2021-03-03 20:10:45 -0800

    add contents for pre lab5, part 1 unfinished

--- phys_book/_build/jupyter_execute/contents/lab5/prelab5.py
+++ phys_book/_build/jupyter_execute/contents/lab5/prelab5.py
@@ -1,3 +1,86 @@
 # Pre Lab 5
 
-## Coming Soon!
+Here are some help for pre lab 5
+___
+
+## Part 1
+
+- Here is an equation to keep in mind for this question.
+$$E_{s} = -\frac{dV}{ds}$$
+
+- The formula is derived as such: 
+
+```{figure} ../../images/lab5/concept_lab5.jpg
+---
+scale: 50%
+---
+
+```
+
+## Part 2
+
+- Our question is:
+
+> Explain why a conductor in electrostatic equilibrium should have no electric field parallel to its 
+> surface, nor within its bulk. (Hint: think about the forces on a free to move cha,rged particle
+
+- Few things to consider about conductors:
+    - <mark>Electrostatic means no <b>net</b> movement of charges.</mark>
+    - <mark>Charges in a conductor are free to move <b>(does not mean they are moving!)</b>.</mark>
+
+- Few basic concepts about electric fields:
+    - Electric field is the distorted space (by a charged source object) that exerts electric force on any charge surrounding charged objects.
+    - **If there is a force that disturbs equilibrium, acceleration is present, meaning things are moving.**
+
+```{tip}
+Use contradiction to prove this!
+```
+-  Now, the question should be clear. Answering this will also give you an idea why the electric field is only available at the surface and perpendicular to the surface.
+
+- A picture for you:
+
+```{figure} ../../images/lab5/conductor.gif
+:scale: 110
+```
+
+```{seealso}
+A conductor is an equipotential surface. That means $\Delta{V} = 0$ everywhere inside it.
+```
+
+
+## Part 3
+
+- Our question is:
+
+> When a steady current I is flowing through a conductor with a non-negligible resistance R, it will be possible to maintain a potential difference $\Delta{V} = I*R$ between parts of the conductor along the direction of the current. Explain why in this situation there should be an electric fieid in
+the direction of the current but not in the direction perpendicular to the current. 
+
+- Now, a few things to consider:
+    - The above concepts about electric fields.
+    - With a current inside a conductor, the charges are moving, **driven by an electric field in the same direction**.
+    - <mark>This field is originated from the potential difference between *upstream* and *downstream* of the source (i.e. battery).</mark>
+
+- A picture for you to understand what I am saying:
+
+```{figure} ../../images/lab5/current_lab5.png
+:scale: 50
+```
+
+- Now, those help answer the first part of the question. The seccond needs some imagination:
+    - <mark>Think of conductor as a metal wire.</mark>
+    - Now, **an electric field perpendicular to the current** will drive the current a way such that it is **closer to the surface**. Right?
+    - Now, let's answer these:
+        - **Can the charge escape the conductor to the environment?**
+        - **If not, how does it behave?**
+```{tip}
+Is there a disturbance to the flow? Anything stuck?
+```
+
+
+## Excel Survey Link
+
+<a target=_blank href=https://ubc.ca1.qualtrics.com/jfe/form/SV_dbztRdUBHaxtNBA>Click Here!</a>
+
+```{attention}
+Make sure to do it so we know what to do!
+```
